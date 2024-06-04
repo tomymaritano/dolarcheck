f65ea51bdc9feaea9faac22823c1491fac413591
Tomás Maritano <tomymaritano@gmail.com>
2024-06-04 10:11:36 -0300

    updates

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -39,7 +39,7 @@ const About = () => {
       <Box
         w="full"
         h="60vh"
-        bgImage="url('https://source.unsplash.com/1600x900/?team')"
+        bgImage="url('https://source.unsplash.com/1600x900/?about')"
         bgPosition="center"
         bgSize="cover"
         display="flex"
