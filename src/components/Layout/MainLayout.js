1a054eb204bec6726eca94b9ea2222524fef8992
Tomás Maritano <tomymaritano@gmail.com>
2024-06-14 20:33:02 -0300

    updates

--- src/components/Layout/MainLayout.js
+++ src/components/Layout/MainLayout.js
@@ -1,22 +1,43 @@
-import { Box, Flex, useDisclosure } from '@chakra-ui/react';
+import React from 'react';
+import { Box, Grid, useDisclosure, useColorModeValue } from '@chakra-ui/react';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import Footer from './Footer';
 
 const MainLayout = ({ children }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const bgColor = useColorModeValue('gray.50', 'gray.900');
 
   return (
-    <Flex direction="column" minH="100vh">
-      <Header onToggleSidebar={onOpen} />
-      <Flex mt={{ base: '0', md: '60px' }}>
+    <Grid
+      templateAreas={{
+        base: `"header" "main" "footer"`,
+        md: `"header header" "sidebar main" "footer footer"`
+      }}
+      gridTemplateRows={{ base: 'auto 1fr auto', md: '60px 1fr auto' }}
+      gridTemplateColumns={{ base: '1fr', md: '200px 1fr' }}
+      minH="100vh"
+      bg={bgColor}
+    >
+      <Box gridArea="header">
+        <Header onToggleSidebar={onOpen} />
+      </Box>
+      <Box
+        gridArea="sidebar"
+        display={{ base: 'none', md: 'block' }}
+        bg={useColorModeValue('white', 'gray.800')}
+        p={4}
+        w="200px"
+      >
         <Sidebar isOpen={isOpen} onClose={onClose} />
-        <Box ml={{ base: 0, md: '200px' }} p={4} flex="1">
-          {children}
-        </Box>
-      </Flex>
-      <Footer />
-    </Flex>
+      </Box>
+      <Box gridArea="main" p={4}>
+        {children}
+      </Box>
+      <Box gridArea="footer">
+        <Footer />
+      </Box>
+    </Grid>
   );
 };
 
